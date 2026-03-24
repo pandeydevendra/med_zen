6fa00749aa4dc1dbf5d0d0a23c191382f364f282
Devendra Pandey <devendrapandey125@gmail.com>
2026-03-24 10:02:07 +0000

    [Devendra]: backend work phase-I

---
+++ backend/main.py
@@ -0,0 +1,15 @@
+from fastapi import FastAPI
+
+app = FastAPI()
+
+@app.get("/")
+def read_root():
+    return {"message": "Hello World"}
+
+
+@app.get("/sum")
+def sum_numbers(a: int, b: int):
+    print(f" Input: {locals()}")
+    result = a + b
+    print(f" Result: {result}")   
+    return {"a": a, "b": b, "sum": result}
